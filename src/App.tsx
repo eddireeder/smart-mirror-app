6c4c2884a1e1837be88fd9fbbe4cf5ae129b6844
Eddie Reeder <e.reeder1997@gmail.com>
2020-10-30 14:51:18 +0000

    Add wake lock request

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import Greeting from "./components/Greeting/Greeting";
 
 function App() {
+  useEffect(() => {
+    requestWakeLock();
+  }, []);
+
+  const requestWakeLock = async () => {
+    try {
+      await (navigator as any).wakeLock.request('screen');
+    } catch (err) {
+      console.error(`${err.name}, ${err.message}`);
+    }
+  }
+
   return (
     <div className="App">
       <Greeting name="Eddie"/>
